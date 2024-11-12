e17eec08f83dcebefdebb2e570a401e1dcfe9f57
Ethen1264 <jxorhks86909763@gmail.com>
2024-11-12 12:05:43 +0900

    💚Fix: cicd

--- src/app/(page)/music/page.tsx
+++ src/app/(page)/music/page.tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/common/Header';
 import TableForm from '@/components/common/Table/Organisms/TableForm';
 
 export default function Music() {
   return (
     <div>
-      {/* <Header /> */}
       <TableForm />
     </div>
   );
--- src/components/common/Table/atoms/Env/index.tsx
+++ src/components/common/Table/atoms/Env/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { State } from '@/assets/svg';
 
-const Env = ({ value }: { value: string }) => {
+const Env = ({ value: _ }: { value: string }) => {
   return (
     <div className="flex w-fit items-center gap-2 rounded-[39px] border-1 border-neutral-n20 px-3 py-1">
       <State />
